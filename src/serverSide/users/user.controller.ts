d1b68d14ffd330de4dfad5d079a56327852ffb3a
severoemanuel <fs.severoemanuel@gmail.com>
2021-12-11 11:18:09 -0300

    feat: backend user crud

--- src/serverSide/users/user.controller.ts
+++ src/serverSide/users/user.controller.ts
@@ -1,6 +1,14 @@
-import type { NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-import type { IRequestCreateUserDto, IRequestUserDto, IResponseUserDto } from './user.dto'
+import ErrorApi from '../ErrorApi'
+import { PaginationQueryDto } from '../pagination/pagination.dto'
+import type {
+  IRequestCreateUserDto,
+  IRequestUpdateUserDto,
+  IRequestUserDto,
+  IResponsePaginateUserDto,
+  IResponseUserDto
+} from './user.dto'
 import type { IUserService } from './user.service'
 
 function create(userService: IUserService) {
@@ -10,17 +18,54 @@ function create(userService: IUserService) {
   }
 }
 
+function update(userService: IUserService) {
+  return async (req: IRequestUpdateUserDto, res: NextApiResponse) => {
+    const { query, body } = req
+    const { userId } = req.auth
+
+    const id = query?.id ? parseInt(query?.id) || 0 : 0
+    if (!userId) throw ErrorApi({ status: 401, message: 'User not logged' })
+
+    await userService.update(id, body)
+    return res.status(201).json({ success: true })
+  }
+}
+
 function findOne(userService: IUserService) {
   return async (req: IRequestUserDto, res: NextApiResponse<IResponseUserDto>) => {
-    const { userId } = req.query
-    const user = await userService.findOne(userId)
-    return res.status(201).json({ success: true, user })
+    const { id } = req.query
+    const user = await userService.findOne(id)
+    return res.status(200).json({ success: true, user })
+  }
+}
+
+function paginate(userService: IUserService) {
+  return async (req: NextApiRequest, res: NextApiResponse<IResponsePaginateUserDto>) => {
+    const { page, size, orderBy, order, ...filter } = req.query as PaginationQueryDto
+    const paginateData = { page, size, orderBy, order }
+
+    const { data, ...pagination } = await userService.paginate(paginateData, filter)
+    return res.status(200).json({ success: true, data, ...pagination })
+  }
+}
+
+function remove(userService: IUserService) {
+  return async (req: IRequestUserDto, res: NextApiResponse<IResponseUserDto>) => {
+    const { query } = req
+
+    const userId = query?.id ? parseInt(`${query.id || 0}`) || 0 : 0
+    const success = await userService.deleteUser(userId)
+
+    return res.status(202).json({ success, userId })
   }
 }
 
 export function factoryUserController(userService: IUserService) {
   return {
     create: create(userService),
-    findOne: findOne(userService)
+    findOne: findOne(userService),
+    paginate: paginate(userService),
+    update: update(userService),
+    remove: remove(userService)
   }
 }
--- src/serverSide/users/user.service.ts
+++ src/serverSide/users/user.service.ts
@@ -1,10 +1,17 @@
-import { User } from '.prisma/client'
+import { User, Prisma as PrismaTypes } from '.prisma/client'
 
 import prisma from '../database/prisma'
-import type { ICreateUserDto } from './user.dto'
+import { PaginationDto, PaginationQueryDto } from '../pagination/pagination.dto'
+import { PrismaService } from '../pagination/pagination.service'
+import type { ICreateUserDto, IUserFilter } from './user.dto'
 
 async function create(data: ICreateUserDto): Promise<number> {
   const user = await prisma.user.create({ data })
+  return user && user.id
+}
+
+async function update(userId: number, data: Partial<User>): Promise<number> {
+  const user = await prisma.user.update({ data, where: { id: userId } })
   return user && user.id
 }
 
@@ -13,9 +20,47 @@ async function findOne(userId: number): Promise<User> {
   return user
 }
 
+async function paginate(pagination: PaginationQueryDto, filter: IUserFilter = {}): Promise<PaginationDto<User>> {
+  const { search, actived } = filter
+  const where: PrismaTypes.UserWhereInput = { id: { not: 0 }, actived }
+
+  if (search)
+    where.AND = {
+      OR: [
+        { name: { contains: `${search}` } },
+        { cellPhone: { contains: `${search}` } },
+        { email: { contains: `${search}` } }
+      ]
+    }
+
+  const users = await PrismaService.paginate<User>({
+    model: 'User',
+    ...pagination,
+    where
+  })
+  return users
+}
+
+async function deleteUser(userId: number, force = false): Promise<boolean> {
+  try {
+    const where: PrismaTypes.UserWhereInput & PrismaTypes.UserWhereUniqueInput = { id: userId }
+
+    if (force) await prisma.user.delete({ where })
+    else await prisma.user.update({ data: { actived: false }, where })
+
+    return true
+  } catch (err) {
+    console.log(err)
+    return false
+  }
+}
+
 export const UserService = {
   create,
-  findOne
+  update,
+  findOne,
+  paginate,
+  deleteUser
 }
 
 export type IUserService = typeof UserService
