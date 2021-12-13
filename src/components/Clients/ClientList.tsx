082afa638103ee52321d3416a12eafff737ecddc
severoemanuel <fs.severoemanuel@gmail.com>
2021-12-13 12:11:54 -0300

    refactor: unique form for edit and add client

--- src/components/Clients/ClientList.tsx
+++ src/components/Clients/ClientList.tsx
@@ -10,10 +10,26 @@ import { Container } from '../Container'
 import { Paragraph } from '../shared/web/src/styled'
 import { ClientItemMemo } from './ClientItem'
 
-interface Props {}
+import { Modal } from '@mui/material'
 
-export const ClientList: React.FC<Props> = () => {
-  const { pagination, data, fetchMoreData, hasMore } = usePagination<Client>()
+import { ModalForm } from '../ModalForm'
+import { ClientForm } from './ClientForm'
+
+interface Props {
+  toggleModal: (id?: number) => void
+  modal: { show: boolean; id: number }
+}
+
+export const ClientList: React.FC<Props> = ({ toggleModal, modal }) => {
+  const { pagination, data, fetchMoreData, hasMore, refreshData } = usePagination<Client>()
+
+  const handleEdit = useCallback(
+    (id = 0) => {
+      if (toggleModal) toggleModal(id)
+      refreshData()
+    },
+    [toggleModal, refreshData]
+  )
 
   const renderMessage = useCallback((text: string) => {
     return (
@@ -24,23 +40,32 @@ export const ClientList: React.FC<Props> = () => {
   }, [])
 
   return (
-    <div>
-      <InfiniteScroll
-        dataLength={data.length}
-        scrollableTarget="layout-container"
-        hasMore={!!hasMore}
-        next={fetchMoreData}
-        loader={
-          <Container>
-            {pagination.page > 1 ? <CircleLoading size={20} relative backgroundColor="#0000" /> : null}
-          </Container>
-        }
-        endMessage={renderMessage('FIM')}
-      >
-        {data?.map?.(item => {
-          return <ClientItemMemo key={`${item.id}`} {...item} />
-        })}
-      </InfiniteScroll>
-    </div>
+    <>
+      <div>
+        <InfiniteScroll
+          dataLength={data.length}
+          scrollableTarget="layout-container"
+          hasMore={!!hasMore}
+          next={fetchMoreData}
+          loader={
+            <Container>
+              {pagination.page > 1 ? <CircleLoading size={20} relative backgroundColor="#0000" /> : null}
+            </Container>
+          }
+          endMessage={renderMessage('FIM')}
+        >
+          {data?.map?.(item => {
+            return <ClientItemMemo key={`${item.id}`} {...item} showModal={modal.show} toggleModal={handleEdit} />
+          })}
+        </InfiniteScroll>
+      </div>
+      <Modal open={modal.show} onClose={toggleModal}>
+        <div>
+          <ModalForm title={'Adicionar cliente'}>
+            <ClientForm clientId={modal.id} onCancel={toggleModal} onSuccess={handleEdit} />
+          </ModalForm>
+        </div>
+      </Modal>
+    </>
   )
 }
--- src/pages/admin/clients.tsx
+++ src/pages/admin/clients.tsx
@@ -12,17 +12,17 @@ import { PageTitle } from '~/components/PageTitle'
 import { PaginationProvider } from '~/components/Providers/PaginationProvider'
 
 const PageAdminClients: NextPage = () => {
-  const [openAddModal, setAddModal] = useState(false)
-  const handleAddClose = () => setAddModal(false)
-  const handleAddOpen = () => setAddModal(true)
+  const [modal, setModal] = useState({ show: false, id: 0 })
+
+  const handleToggleModal = (id = 0) => setModal(old => ({ id, show: !old.show }))
 
   return (
     <LayoutAdmin>
-      <PaginationProvider url="/clients" initialFilter={{}}>
+      <PaginationProvider url="/clients" initialFilter={{ actived: true }}>
         <Container>
           <PageTitle spotlight="Clientes" title="cadastrados" description={'Lista de clientes cadastrados no sistema.'}>
             <Toolbar>
-              <IconButton onClick={handleAddOpen}>
+              <IconButton onClick={() => handleToggleModal()}>
                 <Add />
               </IconButton>
             </Toolbar>
@@ -32,16 +32,9 @@ const PageAdminClients: NextPage = () => {
           <ClientSearch />
         </Container>
         <Container>
-          <ClientList />
+          <ClientList modal={modal} toggleModal={handleToggleModal} />
         </Container>
       </PaginationProvider>
-      <Modal open={openAddModal} onClose={handleAddClose}>
-        <div>
-          <ModalForm title={'Incluir cliente'}>
-            <ClientForm onCancel={handleAddClose} />
-          </ModalForm>
-        </div>
-      </Modal>
     </LayoutAdmin>
   )
 }
