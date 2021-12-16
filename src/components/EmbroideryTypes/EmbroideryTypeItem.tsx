03089a073c4bdd4137a28055c140378b9d00ea8a
severoemanuel <fs.severoemanuel@gmail.com>
2021-12-16 14:33:51 -0300

    feat: card item for embroidery type list

--- src/components/EmbroideryTypes/EmbroideryTypeItem.tsx
+++ src/components/EmbroideryTypes/EmbroideryTypeItem.tsx
@@ -1,22 +1,28 @@
-import { EmbroideryType } from '.prisma/client'
-
-import { Edit } from '@mui/icons-material'
-import { IconButton, Switch, Typography } from '@mui/material'
+import Edit from '@mui/icons-material/Edit'
+import { CardActions, CardContent, IconButton, Switch, Typography } from '@mui/material'
+import { EmbroideryType } from '@prisma/client'
 import { memo, useCallback, useState } from 'react'
+import styled, { css, keyframes } from 'styled-components'
 
 import { useIsMounted } from '~/hooks/useIsMounted'
 import { api } from '~/services/api'
 
-import { FlatItem, FlatDescriptionContainer, FlatDescriptionLine, FlatTitle, FlatText } from '../ListItems/FlatItem'
-
-import styled from 'styled-components'
+import { CardItem } from '../ListItems/CardItem'
 
 interface Props extends EmbroideryType {
   showModal: boolean
   toggleModal: (id?: number) => void
 }
 
-export const EmbroideryTypeItem: React.FC<Props> = ({ id, label, description, actived, showModal, toggleModal }) => {
+export const EmbroideryTypeItem: React.FC<Props> = ({
+  id,
+  label,
+  description,
+  actived,
+  showModal,
+  toggleModal,
+  image
+}) => {
   const [loading, setLoading] = useState(false)
   const [itemActived, setItemActived] = useState(actived)
   const isMounted = useIsMounted()
@@ -38,36 +44,53 @@ export const EmbroideryTypeItem: React.FC<Props> = ({ id, label, description, ac
 
   return (
     <>
-      <FlatItem>
-        <FlatDescriptionContainer style={{ padding: 10 }} grow={1}>
-          <FlatDescriptionLine>
-            <FlatTitle>{label}</FlatTitle>
-          </FlatDescriptionLine>
-          <FlatDescriptionLine>
-            <FlatText>{description}</FlatText>
-          </FlatDescriptionLine>
-        </FlatDescriptionContainer>
-        <IconButton color="primary" onClick={() => toggleModal(id)} disabled={!!showModal}>
-          <Edit />
-        </IconButton>
-        <SwitchContainer>
-          <Switch name="actived" checked={itemActived} color="info" onChange={toggleActived} disabled={loading} />
-          <Typography variant="caption" color="GrayText" htmlFor="actived" component="label">
-            ativo
+      <CardItem sx={{ width: 200 }}>
+        {/* <CardMedia image={image || '/logo64.png'} component="img" alt={label} height={200}  /> */}
+        <ImageContainer src={image || '/logo250.png'} alt={label} />
+        <CardContentTest hasContent={!!description}>
+          <Typography variant="h6">{label}</Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
           </Typography>
-        </SwitchContainer>
-      </FlatItem>
+        </CardContentTest>
+        <CardActions disableSpacing>
+          <IconButton color="primary" onClick={() => toggleModal(id)} disabled={!!showModal}>
+            <Edit />
+          </IconButton>
+          <SwitchContainer>
+            <Switch name="actived" checked={itemActived} color="info" onChange={toggleActived} disabled={loading} />
+            <Typography variant="caption" color="GrayText" htmlFor="actived" component="label">
+              ativo
+            </Typography>
+          </SwitchContainer>
+        </CardActions>
+      </CardItem>
     </>
   )
 }
 
-export const EmbroideryTypeItemMemo = memo(EmbroideryTypeItem) as typeof EmbroideryTypeItem
+export const EmbroideryTypeItemTestMemo = memo(EmbroideryTypeItem)
 
 const SwitchContainer = styled.div`
   display: flex;
   flex-flow: column wrap;
-  align-items: center;
-  justify-content: center;
-  padding: 4;
+  align-items: flex-end;
+  justify-content: flex-end;
+  padding: 4px;
+  flex: 1;
   height: 100%;
+`
+
+const ImageContainer = styled.img`
+  width: 100%;
+  object-fit: contain;
+  height: 200px;
+  display: block;
+  position: relative;
+  margin: 0 auto;
+`
+
+const CardContentTest = styled(CardContent)<{ hasContent?: boolean }>`
+  height: 86px;
+  overflow: hidden;
 `
---
+++ src/components/EmbroideryTypes/EmbroideryTypeItem_old.tsx
@@ -0,0 +1,73 @@
+import { EmbroideryType } from '.prisma/client'
+
+import { Edit } from '@mui/icons-material'
+import { IconButton, Switch, Typography } from '@mui/material'
+import { memo, useCallback, useState } from 'react'
+
+import { useIsMounted } from '~/hooks/useIsMounted'
+import { api } from '~/services/api'
+
+import { FlatItem, FlatDescriptionContainer, FlatDescriptionLine, FlatTitle, FlatText } from '../ListItems/FlatItem'
+
+import styled from 'styled-components'
+
+interface Props extends EmbroideryType {
+  showModal: boolean
+  toggleModal: (id?: number) => void
+}
+
+export const EmbroideryTypeItem: React.FC<Props> = ({ id, label, description, actived, showModal, toggleModal }) => {
+  const [loading, setLoading] = useState(false)
+  const [itemActived, setItemActived] = useState(actived)
+  const isMounted = useIsMounted()
+
+  const toggleActived = useCallback(
+    async e => {
+      const newActived = e.target.checked
+      setItemActived(newActived)
+
+      setLoading(true)
+
+      await api.put(`/embroidery/types/${id}`, { actived: newActived })
+      if (isMounted.current) {
+        setLoading(false)
+      }
+    },
+    [isMounted, id]
+  )
+
+  return (
+    <>
+      <FlatItem>
+        <FlatDescriptionContainer style={{ padding: 10 }} grow={1}>
+          <FlatDescriptionLine>
+            <FlatTitle>{label}</FlatTitle>
+          </FlatDescriptionLine>
+          <FlatDescriptionLine>
+            <FlatText>{description}</FlatText>
+          </FlatDescriptionLine>
+        </FlatDescriptionContainer>
+        <IconButton color="primary" onClick={() => toggleModal(id)} disabled={!!showModal}>
+          <Edit />
+        </IconButton>
+        <SwitchContainer>
+          <Switch name="actived" checked={itemActived} color="info" onChange={toggleActived} disabled={loading} />
+          <Typography variant="caption" color="GrayText" htmlFor="actived" component="label">
+            ativo
+          </Typography>
+        </SwitchContainer>
+      </FlatItem>
+    </>
+  )
+}
+
+export const EmbroideryTypeItemMemo = memo(EmbroideryTypeItem) as typeof EmbroideryTypeItem
+
+const SwitchContainer = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  justify-content: center;
+  padding: 4;
+  height: 100%;
+`
