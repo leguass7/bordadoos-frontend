0d1479d12d31b92166eae9843acdb472bb56156d
severoemanuel <fs.severoemanuel@gmail.com>
2021-12-16 15:27:00 -0300

    refactor: renamed embroidery types folder in server side and changed its name in api

--- src/pages/api/embroidery/types/[embTypeId].ts
+++ src/pages/api/embroidery/types/[embTypeId].ts
@@ -1,9 +1,9 @@
 import nc from 'next-connect'
 
 import { authProtect } from '~/serverSide/auth/auth-protect.middleware'
-import { factoryEmbroideryTypeController } from '~/serverSide/embroiderytypes/embroideryType.controller'
-import { EmbroideryTypeService } from '~/serverSide/embroiderytypes/embroideryType.service'
-import { embtypeValidation } from '~/serverSide/embroiderytypes/embroideryType.validation'
+import { factoryEmbroideryTypeController } from '~/serverSide/embroideryTypes/embroideryType.controller'
+import { EmbroideryTypeService } from '~/serverSide/embroideryTypes/embroideryType.service'
+import { embtypeValidation } from '~/serverSide/embroideryTypes/embroideryType.validation'
 import { ncConfig } from '~/serverSide/ErrorApi'
 
 const controller = factoryEmbroideryTypeController(EmbroideryTypeService)
