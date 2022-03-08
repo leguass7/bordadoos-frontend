d76d207c01f0e35f5d65387e4bc8b14ab3508c20
severoemanuel <fs.severoemanuel@gmail.com>
2022-03-07 21:49:40 -0300

    fix: switch changed to controlled

--- src/components/Form/Switch.tsx
+++ src/components/Form/Switch.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
-import { SwitchProps, Switch as MuiSwitch, Typography } from '@mui/material'
+import { SwitchProps, Switch as MuiSwitch, Typography, FormGroup, FormControlLabel } from '@mui/material'
 import { useField } from '@unform/core'
 
 interface Props extends SwitchProps {
@@ -8,29 +8,71 @@ interface Props extends SwitchProps {
   label?: string
 }
 
-export const Switch: React.FC<Props> = ({ name, label, ...props }) => {
+export const Switch: React.FC<Props> = ({ name, label, onChange, ...props }) => {
   const ref = useRef(null)
   const { defaultValue, fieldName, registerField } = useField(name)
+  const [checked, setChecked] = useState(!!defaultValue)
+  const [started, setStarted] = useState(false)
+  const id = `id-${name}`
+
+  // useEffect(() => {
+  //   if (ref.current) {
+  //     registerField({
+  //       name: fieldName,
+  //       ref: ref.current,
+  //       path: 'checked',
+  //       setValue(r, value: boolean) {
+  //         if (ref.current) setChecked(value)
+  //       },
+  //       clearValue() {
+  //         if (ref.current) setChecked(ref.current?.checked)
+  //       }
+  //     })
+  //   }
+  // }, [registerField, fieldName, checked])
+
+  const handleSelection: any = (event: any) => setChecked(!!event.target.checked)
 
   useEffect(() => {
-    registerField({
-      name: fieldName,
-      ref: ref.current,
-      path: 'checked'
-      // clearValue() {
-      //   if (ref.current?.checked) ref.current.checked = false
-      // }
-    })
-  }, [registerField, fieldName])
+    if (ref.current) {
+      registerField({
+        name: fieldName,
+        ref: ref.current,
+        path: 'checked',
+        setValue: (r, value: boolean) => {
+          setChecked(value)
+          if (r.current) r.current.checked = value
+        },
+        clearValue() {
+          setChecked(false)
+        }
+      })
+    }
+  }, [fieldName, registerField, checked])
+
+  useEffect(() => {
+    if (!started && typeof defaultValue !== 'undefined') {
+      setChecked(!!defaultValue)
+      setStarted(true)
+    }
+  }, [started, defaultValue])
 
   return (
-    <div>
-      {label ? (
-        <Typography component="label" variant="caption" color="grayText">
-          {label}
-        </Typography>
-      ) : null}
-      <MuiSwitch inputRef={ref} defaultChecked={!!defaultValue} {...props} />
-    </div>
+    <FormGroup>
+      <FormControlLabel
+        control={
+          <MuiSwitch
+            id={id}
+            inputRef={ref}
+            checked={!!checked}
+            defaultChecked={defaultValue}
+            onChange={handleSelection}
+            {...props}
+          />
+        }
+        htmlFor={id}
+        label={label}
+      />
+    </FormGroup>
   )
 }
