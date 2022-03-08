326bc34ba51ab4b2638699922cf93c710cc6f6a4
severoemanuel <fs.severoemanuel@gmail.com>
2022-03-07 21:47:10 -0300

    fix: validation of update purchase

--- src/serverSide/purchases/purchase.controller.ts
+++ src/serverSide/purchases/purchase.controller.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { formatDatesOfObject, removeInvalidValues } from '~/helpers/object'
+
 import { IResponseApi } from '../api.interface'
 import ErrorApi from '../ErrorApi'
 import { PaginationQueryDto } from '../pagination/pagination.dto'
@@ -9,8 +11,9 @@ import type { IPurchaseService } from './purchase.service'
 function create(purchaseService: IPurchaseService) {
   return async (req: IRequestFilter, res: NextApiResponse<IResponsePurchase>) => {
     const { userId } = req.auth
+    const data: any = { ...req.body, updatedBy: userId, createdBy: userId }
 
-    const purchase = await purchaseService.create({ ...req.body, updatedBy: userId, createdBy: userId })
+    const purchase = await purchaseService.create(data)
     if (!purchase) throw ErrorApi({ status: 500, message: 'error while creating purchase' })
 
     return res.status(201).json({ purchase })
@@ -23,8 +26,9 @@ function update(purchaseService: IPurchaseService) {
     const { purchaseId } = req.query
 
     if (!userId) throw ErrorApi({ status: 401, message: 'User not logged' })
+    const data = { ...req.body, updatedBy: userId }
 
-    const purchase = await purchaseService.update(purchaseId, { ...req.body, updatedBy: userId })
+    const purchase = await purchaseService.update(purchaseId, data)
     return res.status(201).json({ purchase })
   }
 }
@@ -37,6 +41,16 @@ function paginate(purchaseService: IPurchaseService) {
     const { data, ...pagination } = await purchaseService.paginate(paginateData, filter)
 
     return res.status(200).json({ success: true, data, ...pagination })
+  }
+}
+
+function findById(purchaseService: IPurchaseService) {
+  return async (req: IRequestFilter, res: NextApiResponse<IResponsePurchase>) => {
+    const { purchaseId = 0 } = req.query
+
+    const purchase = await purchaseService.findById(purchaseId)
+
+    return res.status(200).json({ success: true, purchase })
   }
 }
 
@@ -57,6 +71,7 @@ export function factoryPurchaseController(purchaseService: IPurchaseService) {
     create: create(purchaseService),
     paginate: paginate(purchaseService),
     update: update(purchaseService),
+    findById: findById(purchaseService),
     remove: remove(purchaseService)
   }
 }
