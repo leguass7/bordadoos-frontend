c28799a60b427d903826ec27d7d7dac20fc89142
severoemanuel <fs.severoemanuel@gmail.com>
2022-04-15 12:52:23 -0300

    fix: remove invalid values helper

--- src/helpers/object.ts
+++ src/helpers/object.ts
@@ -1,10 +1,12 @@
+import { isDefined } from './variables'
+
 export function isObject(item?: any) {
   return item && typeof item === 'object' && !Array.isArray(item) && !(item instanceof Date)
 }
 
 export function removeInvalidValues<T = unknown>(obj: Record<string, any>): T {
   return Object.entries(obj).reduce((acc, [key, value]) => {
-    if (value) {
+    if (isDefined(value)) {
       if (isNaN(value)) return acc
       acc[key] = value
     }
