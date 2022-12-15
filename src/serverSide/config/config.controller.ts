4a2da4f671448455818533f3a6787b11b5dd6cf6
severoemanuel <46508229+severoemanuel@users.noreply.github.com>
2022-12-14 22:20:55 -0300

    feat(api): added save and find config routes and methods

---
+++ src/serverSide/config/config.controller.ts
@@ -0,0 +1,33 @@
+import type { NextApiResponse } from 'next'
+
+import { IRequestConfigFilter, IRequestSaveConfigDTO } from './config.dto'
+import { IConfigService } from './config.service'
+
+function findOne(configService: IConfigService) {
+  return async (req: IRequestConfigFilter, res: NextApiResponse) => {
+    const { key } = req.query
+    const data = await configService.getOne(key)
+
+    return res.status(201).json({ success: true, data })
+  }
+}
+
+function save(configService: IConfigService) {
+  return async (req: IRequestSaveConfigDTO, res: NextApiResponse) => {
+    const { userId } = req.auth
+
+    const data = await configService.create(req.query?.key, {
+      updatedBy: userId,
+      meta: req.body
+    })
+
+    return res.status(201).json({ success: true, data, message: 'Configurações atualizas com êxito' })
+  }
+}
+
+export function factoryConfigController(userService: IConfigService) {
+  return {
+    save: save(userService),
+    findOne: findOne(userService)
+  }
+}
