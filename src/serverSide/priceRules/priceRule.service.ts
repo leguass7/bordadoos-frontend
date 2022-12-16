57a6af3f5d7ea3dd0850e87b1b0ff8d8c85c0a55
severoemanuel <46508229+severoemanuel@users.noreply.github.com>
2022-12-16 16:42:32 -0300

    feat: update duplicates in bulk create price rules service

--- src/serverSide/priceRules/priceRule.service.ts
+++ src/serverSide/priceRules/priceRule.service.ts
@@ -17,20 +17,48 @@ async function deleteRule(id: number, userId: number) {
   return rule
 }
 
+async function bulkUpdateRules(data: PriceRules[], userId: number) {
+  const ids = data.map(d => d.id)
+
+  const rules = await prisma.priceRules.findMany({ where: { id: { in: ids } } })
+
+  const updated = data.filter(d => {
+    const found = rules.find(r => d.id === r.id)
+    const hasDifferentValue = Object.keys(found).filter(key => found[key] !== d[key])
+
+    if (!!hasDifferentValue?.length) return d
+  })
+
+  const promises = updated.map(up => {
+    return prisma.priceRules.update({ where: { id: up.id }, data: { ...up, updatedBy: userId } })
+  })
+
+  const result = await Promise.all(promises)
+
+  return result
+}
+
 async function bulkCreateRules(data: PriceRules[], userId: number) {
   const dataWithIds = data.map(d => ({ ...d, createdBy: userId, updatedBy: userId }))
+  const duplicates = data.filter(d => !!d?.id)
+
+  await bulkUpdateRules(duplicates, userId)
+  const notDuplicates = dataWithIds.filter(d => !d?.id)
+
   const rules = await prisma.priceRules.createMany({
-    skipDuplicates: true,
-    data: dataWithIds
+    data: notDuplicates
   })
+
   if (!rules) throw ErrorApi('Erro na atualização das regras')
+
   return rules
 }
 
 export const priceRuleService = {
   listRules,
   bulkCreateRules,
-  deleteRule
+  deleteRule,
+  bulkUpdateRules
 }
 
 export type IPriceRuleService = typeof priceRuleService
