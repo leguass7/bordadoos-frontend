71d9a48213ce16764cdeefbcfbea5c64431f197e
severoemanuel <46508229+severoemanuel@users.noreply.github.com>
2022-12-23 00:25:24 -0300

    refactor: updated use has access hook

--- src/hooks/useHasAccess.tsx
+++ src/hooks/useHasAccess.tsx
@@ -5,5 +5,5 @@ import { useSession } from 'next-auth/client'
 export const useHasAccess = (level: number) => {
   const [session] = useSession()
 
-  return useCallback(() => !!(session?.user?.level > level), [level, session?.user?.level])
+  return useCallback(() => !!(session?.user?.level >= level), [level, session?.user?.level])
 }
