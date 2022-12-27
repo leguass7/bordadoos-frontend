6c58e75597c9dd91d2cea4369ef0463baa3be26f
severoemanuel <46508229+severoemanuel@users.noreply.github.com>
2022-12-27 20:22:57 -0300

    refactor: changed prisma models to adapt to new next auth version

---
+++ prisma/migrations/20221227231420_update_next_auth_models/migration.sql
@@ -0,0 +1,109 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `accessTokenExpires` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `compoundId` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `providerAccountId` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `providerId` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `providerType` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshToken` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `userId` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `accessToken` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `sessionToken` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `userId` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `emailVerified` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the `verification_requests` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[provider,provider_account_id]` on the table `accounts` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[session_token]` on the table `sessions` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `provider` to the `accounts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `provider_account_id` to the `accounts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `accounts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `accounts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `session_token` to the `sessions` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `sessions` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE `accounts` DROP FOREIGN KEY `accounts_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `sessions` DROP FOREIGN KEY `sessions_userId_fkey`;
+
+-- DropIndex
+DROP INDEX `accounts_compoundId_key` ON `accounts`;
+
+-- DropIndex
+DROP INDEX `accounts_providerAccountId_key` ON `accounts`;
+
+-- DropIndex
+DROP INDEX `sessions_accessToken_key` ON `sessions`;
+
+-- DropIndex
+DROP INDEX `sessions_sessionToken_key` ON `sessions`;
+
+-- AlterTable
+ALTER TABLE `accounts` DROP COLUMN `accessToken`,
+    DROP COLUMN `accessTokenExpires`,
+    DROP COLUMN `compoundId`,
+    DROP COLUMN `createdAt`,
+    DROP COLUMN `providerAccountId`,
+    DROP COLUMN `providerId`,
+    DROP COLUMN `providerType`,
+    DROP COLUMN `refreshToken`,
+    DROP COLUMN `updatedAt`,
+    DROP COLUMN `userId`,
+    ADD COLUMN `access_token` TEXT NULL,
+    ADD COLUMN `expires_at` INTEGER NULL,
+    ADD COLUMN `id_token` TEXT NULL,
+    ADD COLUMN `provider` VARCHAR(191) NOT NULL,
+    ADD COLUMN `provider_account_id` VARCHAR(191) NOT NULL,
+    ADD COLUMN `refresh_token` TEXT NULL,
+    ADD COLUMN `scope` VARCHAR(191) NULL,
+    ADD COLUMN `session_state` VARCHAR(191) NULL,
+    ADD COLUMN `token_type` VARCHAR(191) NULL,
+    ADD COLUMN `type` VARCHAR(191) NOT NULL,
+    ADD COLUMN `user_id` INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE `sessions` DROP COLUMN `accessToken`,
+    DROP COLUMN `createdAt`,
+    DROP COLUMN `sessionToken`,
+    DROP COLUMN `updatedAt`,
+    DROP COLUMN `userId`,
+    ADD COLUMN `session_token` VARCHAR(191) NOT NULL,
+    ADD COLUMN `user_id` INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE `users` DROP COLUMN `emailVerified`,
+    ADD COLUMN `email_verified` DATETIME(3) NULL,
+    MODIFY `image` VARCHAR(191) NULL;
+
+-- DropTable
+DROP TABLE `verification_requests`;
+
+-- CreateTable
+CREATE TABLE `verificationtokens` (
+    `identifier` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `verificationtokens_token_key`(`token`),
+    UNIQUE INDEX `verificationtokens_identifier_token_key`(`identifier`, `token`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `accounts_provider_provider_account_id_key` ON `accounts`(`provider`, `provider_account_id`);
+
+-- CreateIndex
+CREATE UNIQUE INDEX `sessions_session_token_key` ON `sessions`(`session_token`);
+
+-- AddForeignKey
+ALTER TABLE `accounts` ADD CONSTRAINT `accounts_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
