45b8e31ea5c1a2583f12683770c49b099561311d
severoemanuel <46508229+severoemanuel@users.noreply.github.com>
2023-03-15 20:25:17 -0300

    feat: create user

--- src/pages/admin/users.tsx
+++ src/pages/admin/users.tsx
@@ -1,12 +1,22 @@
-import { Container } from '@mui/material'
+import { useCallback, useState } from 'react'
+
+import { Container, Modal } from '@mui/material'
 
 import { LayoutAdmin } from '~/components/layouts/LayoutAdmin'
+import { ModalForm } from '~/components/ModalForm'
 import { PageTitle } from '~/components/PageTitle'
 import { PaginationProvider } from '~/components/Providers/PaginationProvider'
+import { UserFilter } from '~/components/Users/UserFilter'
+import { UserForm } from '~/components/Users/UserForm'
 import { UserList } from '~/components/Users/UserList'
-import { UserSearch } from '~/components/Users/UserSearch'
 
 const Users: React.FC = () => {
+  const [openModal, setOpenModal] = useState(false)
+
+  const toggleModal = useCallback(() => {
+    setOpenModal(old => !old)
+  }, [])
+
   return (
     <LayoutAdmin>
       <PaginationProvider url="/users">
@@ -18,11 +28,18 @@ const Users: React.FC = () => {
           />
         </Container>
         <Container>
-          <UserSearch />
+          <UserFilter onAdd={toggleModal} />
         </Container>
         <Container>
           <UserList />
         </Container>
+        <Modal open={!!openModal} onClose={toggleModal}>
+          <div>
+            <ModalForm title={'Cadastrar usuário'}>
+              <UserForm onSuccess={toggleModal} onCancel={toggleModal} />
+            </ModalForm>
+          </div>
+        </Modal>
       </PaginationProvider>
     </LayoutAdmin>
   )
